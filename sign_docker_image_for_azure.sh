dcba8cba3ffab59d42d7c7c56d2c213df708caf3
Nandkumar Mohite <nandkumar.mohite@comau.com>
2026-05-12 18:47:11 +0530

    Added Azur Key

---
+++ sign_docker_image_for_azure.sh
@@ -0,0 +1,33 @@
+!#/bin/bash
+
+sudo update-ca-certificates
+
+DCT_PATH=$1
+USER=comau_signer_ci
+DOCKER_CONTENT_TRUST_ROOT_PASSPHRASE=$2
+DOCKER_CONTENT_TRUST_REPOSITORY_PASSPHRASE=$3
+DOCKER_REPO=$4
+IMAGE_TAG=$5
+PROXY_URL=$6
+ACR_USER=$7
+ACR_PASSWORD=$8
+
+export DOCKER_CONTENT_TRUST=1
+export HTTPS_PROXY=http://$PROXY_URL
+az acr login --name $DOCKER_REPO -u $ACR_USER -p $ACR_PASSWORD
+
+
+grep -r $USER ~/.docker/trust/private/
+if [ "$?" -eq "1" ]
+then 
+  echo "please contact administration to load private key to create the root key"
+  exit 1
+fi
+
+docker trust inspect --pretty $DOCKER_REPO | grep $USER
+if [ "$?" -eq "1" ]
+then
+docker trust signer add --key $DCT_PATH/comau_delegation.crt $USER $DOCKER_REPO
+fi
+
+docker trust sign "$DOCKER_REPO:$IMAGE_TAG"
